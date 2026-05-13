6edbfbfc61660595192ec09c0f8fdea1457bd926
Syed KanzulEman <syedkanzuleman.official@gmail.com>
2026-05-13 15:18:41 +0500

    feat: add register component and update App to include it

--- frontend/auth-frontend/src/App.jsx
+++ frontend/auth-frontend/src/App.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from 'react'
+import register from './components/register'
 
-export default function App() {
+export const App = () => {
   return (
-    <div className='w-full min-h-screen bg-gray-950'>
-      <h1 className='text-3xl font-bold text-center  text-white'>Welcome to the Auth App</h1>
+    <div>
+      <register/>
     </div>
-  );
+  )
 }
